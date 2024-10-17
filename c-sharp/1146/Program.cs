24933140f2ea2c9afe7adddd72647b8dd7fb8137
Silvia Frigatto <59895509+silviafrigatto@users.noreply.github.com>
2024-10-17 12:22:59 -0300

    Add files via upload

---
+++ c-sharp/1146/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://judge.beecrowd.com/en/problems/view/1146
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x;
+		
+		do
+		{
+			x = int.Parse(Console.ReadLine()!);
+
+			for(int y = 1; y <= x; y++)
+			{
+				string output = (x == y) ? y + "\n" : y + " ";
+				Console.Write(output);	
+			}	
+		} 
+		while (x > 0);
+		
+		Environment.Exit(0);
+    }
+}
