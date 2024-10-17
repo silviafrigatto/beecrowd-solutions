8394a5f370f048ff7a95c5a2bbd186a8adaf7735
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-17 18:22:52 -0300

    Add 1132 solution

---
+++ c-sharp/1132/Program.cs
@@ -0,0 +1,27 @@
+﻿// judge.beecrowd.com/en/problems/view/1132
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 0;
+		
+		int x = int.Parse(Console.ReadLine()!);
+		int y = int.Parse(Console.ReadLine()!);
+		
+		int smallest = Math.Min(x, y);
+		int largest = Math.Max(x, y);
+				
+		for (int i = smallest; i <= largest; i++)
+		{
+			
+			if ( i % 13 != 0)
+			{
+				sum += i;
+			}
+			
+		}
+		
+		Console.WriteLine(sum);
+    }
+}
