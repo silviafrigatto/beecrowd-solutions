a4ca2be228f8f92579ab176c0258ca6db7dea0a4
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-17 18:16:41 -0300

    1133 solution

---
+++ c-sharp/1133/Program.cs
@@ -0,0 +1,21 @@
+﻿// judge.beecrowd.com/en/problems/view/1133
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine()!);
+		int y = int.Parse(Console.ReadLine()!);
+		
+		int max = Math.Max(x, y);
+		int min = Math.Min(x, y);
+		
+		for (int i = min + 1; i < max; i++)
+		{
+			if (i % 5 == 2 || i % 5 == 3)
+			{
+				Console.WriteLine(i);
+			}
+		}
+    }
+}
