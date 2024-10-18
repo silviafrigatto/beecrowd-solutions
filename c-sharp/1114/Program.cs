2ae19a2ce324c9eda79052341432afbafe7755ca
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-18 13:07:51 -0300

    Added 1114 solution

---
+++ c-sharp/1114/Program.cs
@@ -0,0 +1,17 @@
+﻿// judge.beecrowd.com/en/problems/view/1114
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int password;
+		
+		do
+		{
+			password = int.Parse(Console.ReadLine()!);
+			string access = password != 2002 ? "Senha Invalida" : "Acesso Permitido";
+			Console.WriteLine(access);
+		}
+		while(password != 2002);
+    }
+}
