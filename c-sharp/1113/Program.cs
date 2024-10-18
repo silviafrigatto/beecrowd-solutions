4416b834c15a53d61fbcd8ea2b1dc866143b2daa
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-18 14:17:08 -0300

    Added 1113 solution

---
+++ c-sharp/1113/Program.cs
@@ -0,0 +1,27 @@
+﻿//judge.beecrowd.com/en/problems/view/1113
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool differentNumbers = true;
+		
+		while(differentNumbers)
+		{
+			string[] input = Console.ReadLine()!.Split();
+			
+			int x = int.Parse(input[0]);
+			int y = int.Parse(input[1]);
+			
+			if (x == y)
+			{
+				differentNumbers = false;
+			}
+			else
+			{
+				string order = x > y ? "Decrescente" : "Crescente";
+				Console.WriteLine(order);
+			}		
+		}
+    }
+}
