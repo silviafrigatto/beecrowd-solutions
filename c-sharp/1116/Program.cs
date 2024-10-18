39a80e678183f18b399ee052b3bcde4882cdbd2a
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-18 13:00:15 -0300

    Added 1116 solution

---
+++ c-sharp/1116/Program.cs
@@ -0,0 +1,28 @@
+﻿// judge.beecrowd.com/en/problems/view/1116
+
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+		
+		for(int i = 0; i < n; i++)
+		{
+			string[] numbers = Console.ReadLine()!.Split();
+			int x = int.Parse(numbers[0]);
+			int y = int.Parse(numbers[1]);
+			
+			if(y == 0)
+			{
+				Console.WriteLine("divisao impossivel");
+			}
+			else
+			{
+				double result = (double) x / y;
+				Console.WriteLine(result.ToString("F1", CultureInfo.InvariantCulture));
+			}
+		}
+    }
+}
