5fe8ed2c00e31954e0e6ef9c13a97af7abe64dd0
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-18 12:54:51 -0300

    Added 1117 solution

---
+++ c-sharp/1117/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int totalValidScores = 0;
+		double sum = 0, average = 0;
+				
+		while(totalValidScores != 2)
+		{
+			double score = double.Parse(Console.ReadLine()!);
+			
+			if(score < 0 || score > 10)
+			{
+				Console.WriteLine("nota invalida");
+			}
+			else
+			{	
+				totalValidScores ++;
+				sum += score;
+			}
+		}
+		
+		average = sum / 2;
+		Console.WriteLine("media = " + average.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
