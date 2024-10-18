ea42f367d2c622cdf41d2663baa8701776a0ddd5
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-10-18 13:04:19 -0300

    Added 1115 solution

---
+++ c-sharp/1115/Program.cs
@@ -0,0 +1,38 @@
+﻿// judge.beecrowd.com/en/problems/view/1115
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x, y;
+		bool notZero = true;
+		
+		while(notZero)
+		{
+			string[] coordinates = Console.ReadLine()!.Split();
+			x = int.Parse(coordinates[0]);
+			y = int.Parse(coordinates[1]);
+			
+			if (x > 0 && y > 0)
+			{
+				Console.WriteLine("primeiro");	
+			}
+			else if (x < 0 && y > 0)
+			{
+				Console.WriteLine("segundo");
+			}
+			else if (x < 0 && y < 0)
+			{
+				Console.WriteLine("terceiro");
+			}
+			else if (x > 0 && y < 0)
+			{
+				Console.WriteLine("quarto");
+			}
+			else
+			{
+				notZero = false;
+			}		
+		}
+    }
+}
