a6501162a69113db00740078c798e3065b068b03
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-11-11 19:47:45 -0300

    Implemented the sum of all consecutive numbers

--- c-sharp/1149/Program.cs
+++ c-sharp/1149/Program.cs
@@ -1,12 +1,29 @@
-<<<<<<< HEAD
-﻿internal class Program
+﻿
+//judge.beecrowd.com/en/problems/view/1149
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int r = 1;
+        int somaDosConsecutivos = 0;
+        string input = Console.ReadLine()!;
+        string[] valores = input.Split(' ');   
+
+
+        if (valores.Length == 2 && int.TryParse(valores[0], out int a) && int.TryParse(valores[1], out int n))
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int termo = a + i * r;  // Formula PA
+                Console.WriteLine(termo);
+                somaDosConsecutivos += termo;
+            } 
+            Console.WriteLine(somaDosConsecutivos);    
+        }   
+        else
+        {
+            Console.WriteLine("Por favor, digite dois números válidos, com um espaço entre eles.");
+        }      
     }
 }
-=======
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
->>>>>>> 24933140f2ea2c9afe7adddd72647b8dd7fb8137
