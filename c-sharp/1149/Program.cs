c6afdf449dd556ad3ffef70b20afdf45724e3748
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-11-14 18:38:18 -0300

    1149 Converting string to array of integers.

--- c-sharp/1149/Program.cs
+++ c-sharp/1149/Program.cs
@@ -7,15 +7,31 @@ internal class Program
     {
         int r = 1;
         int somaDosConsecutivos = 0;
-        string input = Console.ReadLine()!;
-        string[] valores = input.Split(' ');   
+        string inputString = Console.ReadLine()!;
+        string[] valores = inputString.Split(' ');  
+        int[] inputInt = Array.ConvertAll(valores, int.Parse); 
+
+        Console.WriteLine(inputInt[0] + " " + inputInt[1]);
+
+        int a = inputInt[0];
+        int n = inputInt[1];   
+
+        while ( a <= 0 && n <= 0) 
+        {
+            
+        }
 
         if (valores.Length == 2 && int.TryParse(valores[0], out int a) && int.TryParse(valores[1], out int n))
         {
-            do
-            {
-                n = int.Parse(Console.ReadLine()!);
-            } while (n <= 0);
+            // do
+            // {
+            //     n = int.Parse(Console.ReadLine()!);
+            // } while (n <= 0);
+
+            // while (n <= 0)
+            // {
+            //     n = int.Parse(Console.ReadLine()!);
+            // }
 
             for (int i = 0; i < n; i++)
             {
