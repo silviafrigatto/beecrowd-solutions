e0d1f18c4774d602e5ca9d73b41a7e42f0d5d7a8
Silvia Frigatto <silvia.frigatto@gmail.com>
2024-11-26 19:38:01 -0300

    1150 using if else

---
+++ c-sharp/1150/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine()!);
+        int z = int.Parse(Console.ReadLine()!);
+        //int count = 0;
+
+        if(x < z)
+        {
+            // for(count = 0; z > x; count++)
+            // {
+            //     x += 1;
+            // }
+            while(z > x)
+            {
+                x += 1;
+                //count++;
+            }
+            Console.WriteLine(x);
+        }
+        else
+        {
+            while(z <= x)
+            {
+                z = int.Parse(Console.ReadLine()!);
+            }
+        } 
+        
+    }
+}
